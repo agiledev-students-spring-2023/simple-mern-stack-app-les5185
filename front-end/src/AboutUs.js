d35bc6a124e61b6ec6d035ba7fff6e3cf4009b72
les5185 <70672721+les5185@users.noreply.github.com>
2023-02-15 10:45:11 -0500

    Add text and basics

---
+++ front-end/src/AboutUs.js
@@ -0,0 +1,20 @@
+import image from './image.jpg'
+import React from 'react'
+import './AboutUs.css'
+
+const AboutUs = props => {
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <h2>Hi! My name is Tiffany.</h2>
+      <img src={image} />
+      <p>
+        I am a senior studying computer science. I am from Korea. I am
+        interested in entrepreneurship and software development. On my free
+        time, I like to cook, go on a run, and binge-watch Netflix shows.
+      </p>
+    </div>
+  )
+}
+
+export default AboutUs
