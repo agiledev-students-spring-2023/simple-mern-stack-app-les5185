583e6b6f4c3ed023a1805225defd249f46793a27
les5185 <70672721+les5185@users.noreply.github.com>
2023-02-15 11:35:25 -0500

    add style

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -6,12 +6,23 @@ const AboutUs = props => {
   return (
     <div className="about">
       <h1>About Us</h1>
-      <h2>Hi! My name is Tiffany.</h2>
-      <img src={image} width="20%" />
-      <p>
-        I am a senior studying computer science. I am from Korea. I am
-        interested in entrepreneurship and software development. On my free
-        time, I like to cook, go on a run, and binge-watch Netflix shows.
+      <img
+        src={image}
+        style={{
+          width: '25%',
+          display: 'inline-block',
+          verticalAlign: 'top',
+        }}
+      />
+      <p style={{ width: '50%', display: 'inline-block', marginTop: '16px' }}>
+        <h3>
+          Hi! My name is Tiffany. I am a senior studying computer science.{' '}
+          <br /> I am from Korea. I am interested in entrepreneurship and
+          software development. As an aspiring entrepreneur, I wish to help
+          people form healthy values in their lives and stay positive and
+          motivated. <br /> On my free time, I like to cook, go on a run, and
+          binge-watch Netflix shows.
+        </h3>
       </p>
     </div>
   )
